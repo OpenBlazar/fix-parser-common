b4f272ce4d06a13d8c03ed0bfdad5523ef7ca648
Wojciech Zankowski <info@zankowski.pl>
2019-06-16 16:31:25 +0200

    Initial commit

---
+++ src/main/java/pl/zankowski/fixparser/common/RandomUtil.java
@@ -0,0 +1,11 @@
+package pl.zankowski.fixparser.common;
+
+import java.util.UUID;
+
+public class RandomUtil {
+
+    public static String randomUuid() {
+        return UUID.randomUUID().toString();
+    }
+
+}
